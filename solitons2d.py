127b8258fc82bed9a0aef09d5e45c12fac94a58b
Thorsten Buss <thorsten.buss@mathphys.stura.uni-heidelberg.de>
2020-02-22 14:45:50 +0100

    add rand2d via fft clean up file names

--- solitons2d.py
+++ solitons2d.py
@@ -9,35 +9,43 @@ mpl.rcParams.update(pgf_with_rc_fonts)
 import matplotlib.pyplot as plt
 import numpy as np
 
-dase = 'rand'
+dase = 'rand5/'
 L       =  40
-npoints = 600
-dt      =   0.005
+npoints = 512
+dt      =   0.001
 dx      = L/npoints
 
 grid = np.arange(-L/2,L/2,dx,complex)
 grid = np.meshgrid(grid,grid)
 
-def rand(x):
-  ret = 0
-  s = np.random.uniform(0,2*np.pi,40)
-  for i in range(1,21):
-    a = 0.1*np.exp(-i*i/50)
-    ret += a*np.sin(i*x*(2*np.pi)/L+s[i-1])
-  return ret
+def rand1d(x):
+    k  = 2*np.pi*np.fft.fftfreq(npoints,d=dx)
+    e  = -(k*k)/(2*25)
+    r_ = np.exp(e).astype(complex)
+    s  = np.random.uniform(0,1,npoints).astype(complex)
+    r_*= np.exp(1j*2*np.pi*s)
+    r  = np.fft.ifft2(r_)
+    return np.real(r)
 
-def grey_soliton_rand(nu=0.5,z0=0):
+def rand2d():
+    k  = 2*np.pi*np.fft.fftfreq(npoints,d=dx)
+    k  = np.meshgrid(k,k)
+    e  = -(k[0]*k[0]+k[1]*k[1])/(2*20)
+    r_ = np.exp(e).astype(complex)
+    s  = np.random.uniform(0,1,(npoints,npoints)).astype(complex)
+    r_*= np.exp(1j*2*np.pi*s)
+    r  = np.fft.ifft2(r_)
+    return 100*r+1
+
+def grey_soliton_rand_pos(nu=0.,z0=0):
     gamma=1/np.sqrt(1-nu**2)
-    ret = 1j*nu+1/gamma*np.tanh((grid[0]-z0-rand(grid[1]))/gamma)
+    ret = 1j*nu+1/gamma*np.tanh((grid[0]-z0-rand1d(grid[1]))/gamma)
     return ret*np.ones((npoints,npoints))
 
-def grey_soliton(nu=0.5,z0=0):
+def grey_soliton(nu=0.,z0=0):
     gamma=1/np.sqrt(1-nu**2)
-    ret = 1j*nu+1/gamma*np.tanh((grid[0]-z0-0.1*np.sin(grid[1]*(2*np.pi)/L*5))/gamma)
+    ret = 1j*nu+1/gamma*np.tanh((grid[0]-z0)/gamma)
     return ret*np.ones((npoints,npoints))
-
-def dark_soliton(z0=0):
-    return grey_soliton(nu=0,z0=z0)
 
 def ring(R=5,nu=-0.5):
     gamma=1/np.sqrt(1-nu**2)
@@ -66,10 +74,11 @@ def TimeEvolution(psi0, tsteps_, dt_=dt):
         psi  = np.exp(-1j*dt_*ro(psi))*psi
         t += dt_/2
         if i%50==0:
-            np.save('data/{}{}'.format(dase,i//50), np.real(ro(psi)))
+            np.save('data/{}{}'.format(dase,i//50), psi)
             plt.imshow(np.real(ro(psi)), cmap=plt.get_cmap("BuPu"), origin='lower', 
-                extent=[-L/2, L/2-dx, -L/2, L/2-dx])
-            plt.savefig('plots/{}{}.svg'.format(dase,i//50))
+                extent=[-L/2, L/2-dx, -L/2, L/2-dx],vmin=0, vmax=1.1)
+            plt.colorbar()
+            plt.savefig('plots/{}{}.png'.format(dase,i//50))
             plt.close()
     psi_ = np.fft.fft2(psi)
     psi_ = np.exp(-1j*dt*H1)*psi_
@@ -81,8 +90,21 @@ def TimeEvolution(psi0, tsteps_, dt_=dt):
 def main():
     #psi = grey_soliton(0.,-5)*grey_soliton(-0.,5)
     #psi = ring()
-    psi = grey_soliton_rand(0.,-5)*grey_soliton_rand(-0.,5)
-    psi = TimeEvolution(psi, 5001, dt_=dt)
+    #psi = grey_soliton_rand2(0.,-5)*grey_soliton_rand2(-0.,5)
+    r = rand3()
+    plt.imshow(np.real(r), cmap=plt.get_cmap("BuPu"), origin='lower', 
+                extent=[-L/2, L/2-dx, -L/2, L/2-dx])
+    plt.colorbar()
+    plt.savefig('plots/000.svg')
+    plt.close()
+    plt.imshow(np.imag(r), cmap=plt.get_cmap("BuPu"), origin='lower', 
+                extent=[-L/2, L/2-dx, -L/2, L/2-dx])
+    plt.colorbar()
+    plt.savefig('plots/001.svg')
+    plt.close()
+    print("hallo")
+    psi = r*grey_soliton(0.,-10)*grey_soliton(-0.,10)
+    psi = TimeEvolution(psi, 100001, dt_=dt)
 
 if __name__ == "__main__":
     main()
